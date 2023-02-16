d399122b73fbf8ed2223daa0af70ddfcef9e5529
k3vinrich4rd <kevinrichardve@gmail.com>
2023-02-16 13:20:51 -0300

    Inserindo a criação de uma variável global

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,8 +1,18 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.template.AccountSavings
+import br.com.alura.bytebank.template.Address
+import br.com.alura.bytebank.template.Client
+import br.com.alura.bytebank.template.accountsTotal
 import br.com.alura.bytebank.test.testDifferentAccounts
 
 fun main() {
+
+    val test = Client("Mano Brown", "243423", Address(), 411)
+    AccountSavings(test, 5445254)
+
     testDifferentAccounts()
+
+    println("Account total created: $accountsTotal")
 
 }
--- src/main/kotlin/br/com/alura/bytebank/template/Account.kt
+++ src/main/kotlin/br/com/alura/bytebank/template/Account.kt
@@ -1,12 +1,20 @@
 package br.com.alura.bytebank.template
 
+var accountsTotal = 0 //Inicialização da variável global
+    private set //Property global com o set privado
+
+//Fazendo o valor dessa property não ser modificado em qualquer ponto do código
 abstract class Account(
-    val holder: Client,
+    val holder: Client, //Composição
     val numberAccount: Int  // 0 Valor padrão (tirando a necessidade de informar um valor no argumento do constructor
     // só se apenas quiser informar um valor
 ) {
-    var balance = 0.0
+    open var balance = 0.0
         protected set
+
+    init {//Executa um código antes de instânciar qualquer objeto filho de account
+        accountsTotal++ //Incremento da variável global (código)
+    }
 
     fun depositInAnAccount(value: Double) {  //(Função) membro (métodos/comportamento)
         if (value > 0) {
