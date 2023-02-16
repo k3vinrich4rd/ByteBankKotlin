a3d4090148b7938d7b155a48ba5f90ea1d00006a
k3vinrich4rd <kevinrichardve@gmail.com>
2023-02-16 13:54:53 -0300

    Inserindo a exemplo de objeto anônimo

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -8,6 +8,19 @@ import br.com.alura.bytebank.test.testDifferentAccounts
 
 fun main() {
 
+    val fran = object { //Objeto anônimo
+        //Nos permite criar uma instância de um objeto sem necessitar de uma classe
+        //Exemplo:
+        val name: String = "Kevin"
+        val cpf: String = "111.111.111-11"
+        val password: Int = 1000
+
+        fun authenticated(password: Int) = this.password == password
+    }
+
+    println("Nome do cliente: ${fran.name}")
+    println("Nome do cliente: ${fran.authenticated(1000)}\n")
+
     val test = Client("Mano Brown", "243423", Address(), 411)
     AccountSavings(test, 5445254)
 
