3a84204407433c098a4089c1add10f1783f43a75
k3vinrich4rd <kevinrichardve@gmail.com>
2023-02-16 14:02:49 -0300

    Inserindo a exemplo de objeto anônimo implementando uma interface

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,22 +1,23 @@
 package br.com.alura.bytebank
 
-import br.com.alura.bytebank.template.AccountSavings
-import br.com.alura.bytebank.template.Address
-import br.com.alura.bytebank.template.Client
-import br.com.alura.bytebank.template.accountsTotal
+import br.com.alura.bytebank.template.*
 import br.com.alura.bytebank.test.testDifferentAccounts
 
 fun main() {
 
-    val fran = object { //Objeto anônimo
+    val fran = object : Authenticated{ //Objeto anônimo
         //Nos permite criar uma instância de um objeto sem necessitar de uma classe
         //Exemplo:
         val name: String = "Kevin"
         val cpf: String = "111.111.111-11"
         val password: Int = 1000
 
-        fun authenticated(password: Int) = this.password == password
+       override fun authenticated(password: Int) = this.password == password
     }
+    //Implementando uma ‘interface’ com object anônimo
+    //Exemplo:
+    val systemIntern = SystemIntern()
+    systemIntern.toEnter(fran, 1000)
 
     println("Nome do cliente: ${fran.name}")
     println("Nome do cliente: ${fran.authenticated(1000)}\n")
